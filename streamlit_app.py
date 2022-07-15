73fe765e2e93ad897ef921373534ea504e891cc6
Nisha2403 <109325052+Nisha2403@users.noreply.github.com>
2022-07-15 13:45:56 -0400

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
-streamlit.title('My first streamlit app')
+streamlit.title('My first healthy diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Pancakes')
+streamlit.text('Waffels')
+streamlit.text('Orange Juice')
+streamlit.text('Sandwich')
