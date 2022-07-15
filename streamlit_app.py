74615f44d0f63d255b12d140e1f7162e23e996fc
Nisha2403 <109325052+Nisha2403@users.noreply.github.com>
2022-07-15 13:50:31 -0400

    Update streamlit_app.py
    Added icons and another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ streamlit.title('My first healthy diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Pancakes')
-streamlit.text('Waffels')
-streamlit.text('Orange Juice')
-streamlit.text('Sandwich')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avocado Toast')
+
+treamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
